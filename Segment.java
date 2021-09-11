9698fb6082700816464bb0f7ecffc6603521dc0b
David Eisenstat <eisenstatdavid@gmail.com>
2021-09-11 13:27:54 -0400

    Refactor segment tree construction.

--- Segment.java
+++ Segment.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedSet;
+
 public final class Segment {
   public Segment(Interval interval) {
     interval_ = interval;
@@ -8,6 +12,31 @@ public final class Segment {
     leftChild_ = leftChild;
     rightChild_ = rightChild;
     updateChildrenLength();
+  }
+
+  public static Segment newTree(SortedSet<Double> bounds) {
+    List<Segment> segments = new ArrayList<Segment>();
+    double min = Double.NaN;
+    for (double max : bounds) {
+      if (!Double.isNaN(min)) {
+        segments.add(new Segment(new Interval(min, max)));
+      }
+      min = max;
+    }
+    if (segments.isEmpty()) {
+      return null;
+    }
+    while (segments.size() > 1) {
+      List<Segment> parentSegments = new ArrayList<Segment>();
+      for (int i = 1; i < segments.size(); i += 2) {
+        parentSegments.add(new Segment(segments.get(i - 1), segments.get(i)));
+      }
+      if (segments.size() % 2 == 1) {
+        parentSegments.add(segments.get(segments.size() - 1));
+      }
+      segments = parentSegments;
+    }
+    return segments.get(0);
   }
 
   public void add(Interval interval) {
